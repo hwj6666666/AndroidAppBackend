3f508d6525f859717287c10e6cb52672e14f9c90
SundayChen <824987073@qq.com>
2024-07-12 00:35:39 +0800

    security

--- jiaoji_backend/jiaoji/src/main/java/org/example/jiaoji/security/ShiroConfig.java
+++ jiaoji_backend/jiaoji/src/main/java/org/example/jiaoji/security/ShiroConfig.java
@@ -3,14 +3,14 @@ package org.example.jiaoji.security;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import javax.servlet.Filter;
-
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+
+import javax.servlet.Filter;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Configuration
 public class ShiroConfig {
@@ -33,7 +33,7 @@ public class ShiroConfig {
         shiroFilter.setSecurityManager(securityManager);
 
         Map<String, Filter> customFilters = new LinkedHashMap<>();
-        customFilters.put("jwt", new JWTFilter());
+        customFilters.put("auth", new JWTFilter());
         shiroFilter.setFilters(customFilters);
 
         Map<String, String> filterMap = new LinkedHashMap<>();
@@ -46,7 +46,6 @@ public class ShiroConfig {
         return shiroFilter;
     }
 
-    //配置shiro注解支持
     @Bean
     public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
         AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
@@ -54,10 +53,8 @@ public class ShiroConfig {
         return advisor;
     }
 
-    //负责shiro的生命周期
     @Bean("lifecycleBeanPostProcessor")
     public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
         return new LifecycleBeanPostProcessor();
     }
-
 }

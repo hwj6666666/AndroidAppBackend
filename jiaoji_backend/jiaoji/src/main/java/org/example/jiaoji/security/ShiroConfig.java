2773ed555a104ded309e27df98c9e2a561ffeabd
SundayChen <824987073@qq.com>
2024-07-12 11:30:00 +0800

    security

--- jiaoji_backend/jiaoji/src/main/java/org/example/jiaoji/security/ShiroConfig.java
+++ jiaoji_backend/jiaoji/src/main/java/org/example/jiaoji/security/ShiroConfig.java
@@ -1,11 +1,13 @@
 package org.example.jiaoji.security;
 
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.springframework.context.annotation.DependsOn;
 
 import javax.servlet.Filter;
 import java.util.LinkedHashMap;
@@ -14,41 +16,50 @@ import java.util.Map;
 @Configuration
 public class ShiroConfig{
 
-    @Bean
+    @Bean(name = "shiroFilter")
+    @DependsOn("lifecycleBeanPostProcessor")
     public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager) {
         ShiroFilterFactoryBean shiroFilter = new ShiroFilterFactoryBean();
         shiroFilter.setSecurityManager(securityManager);
 
         Map<String, Filter> customFilters = new LinkedHashMap<>();
-        customFilters.put("auth", new JWTFilter());
+        customFilters.put("jwt", new JWTFilter());
         shiroFilter.setFilters(customFilters);
 
         Map<String, String> filterMap = new LinkedHashMap<>();
         filterMap.put("/user/login", "anon");
         filterMap.put("/mail/**", "anon");
         filterMap.put("/user/register", "anon");
-        filterMap.put("/**", "auth");
+        filterMap.put("/**", "jwt");
 
         shiroFilter.setFilterChainDefinitionMap(filterMap);
         return shiroFilter;
     }
 
     @Bean
+    @DependsOn("lifecycleBeanPostProcessor")
     public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
         AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
         advisor.setSecurityManager(securityManager);
         return advisor;
     }
 
-    @Bean
-    public SecurityManager securityManager() {
+    @Bean(name = "securityManager")
+    @DependsOn("lifecycleBeanPostProcessor")
+    public SecurityManager securityManager(MyCustomRealm myCustomRealm) {
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-        securityManager.setRealm(myCustomRealm());
+        securityManager.setRealm(myCustomRealm);
         return securityManager;
     }
 
-    @Bean
+    @Bean(name = "myCustomRealm")
+    @DependsOn("lifecycleBeanPostProcessor")
     public MyCustomRealm myCustomRealm() {
         return new MyCustomRealm();
+    }
+
+    @Bean(name = "lifecycleBeanPostProcessor")
+    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
     }
 }

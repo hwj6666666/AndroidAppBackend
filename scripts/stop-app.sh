01a8f2186e3f00fe6e5dccbca3b18b7699a47a37
spring1chong <spring1chong@sjtu.edu.cn>
2024-07-16 16:54:07 +0800

    cicd

--- scripts/stop-app.sh
+++ scripts/stop-app.sh
@@ -7,6 +7,6 @@ cd /www/wwwroot/jiaoji
 pkill -f 'java -jar jiaoji-0.0.1-SNAPSHOT.jar'
 
 # 备份旧版本的JAR文件
-if [ -f "jiaoji-0.0.1-SNAPSHOT.jar" ]; then
-  mv jiaoji-0.0.1-SNAPSHOT.jar jiaoji.jar.bak_$(date +%Y%m%d%H%M%S)
-fi
+# if [ -f "jiaoji-0.0.1-SNAPSHOT.jar" ]; then
+#   mv jiaoji-0.0.1-SNAPSHOT.jar jiaoji.jar.bak_$(date +%Y%m%d%H%M%S)
+# fi

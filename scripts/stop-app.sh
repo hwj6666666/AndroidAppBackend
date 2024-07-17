996f73cbeb7d36ca6ac377e2b430e2f878c56329
spring1chong <spring1chong@sjtu.edu.cn>
2024-07-17 11:17:46 +0800

    cicd

--- scripts/stop-app.sh
+++ scripts/stop-app.sh
@@ -4,11 +4,11 @@
 cd /www/wwwroot/jiaoji
 
 # 停止旧的进程
-pkill -f 'java -jar jiaoji-0.0.1-SNAPSHOT.jar'
-#if [ $? -ne 0 ]; then
-#  echo "Failed to stop the old application process."
-#  exit 1
-#fi
+pkill -f 'java -jar jiaoji-0.0.1-SNAPSHOT.jar' || echo "No old application process found to stop."
+
+# 如果需要详细调试信息，可以取消以下注释以查看当前运行的Java进程
+# echo "Current Java processes:"
+# ps aux | grep java
 
 # 备份旧版本的JAR文件
 # if [ -f "jiaoji-0.0.1-SNAPSHOT.jar" ]; then

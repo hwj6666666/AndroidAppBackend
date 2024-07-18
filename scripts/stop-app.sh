ee0ed0faa0394cdaef359a12264a18fde0132216
spring1chong <spring1chong@sjtu.edu.cn>
2024-07-18 11:21:14 +0800

    cicd

--- scripts/stop-app.sh
+++ scripts/stop-app.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 # 进入应用程序目录
-cd /www/wwwroot/jiaoji || exit
+cd /www/wwwroot/jiaoji || { echo "Failed to change directory to /www/wwwroot/jiaoji"; exit 1; }
 
-# 查找绑定到指定端口的进程并结束它
+# 查找绑定到指定端口的进程并尝试结束它
 pid=$(lsof -ti:6981)
-if [ -n "$pid" ]; then
+while [ -n "$pid" ]; do
+    echo "Stopping process $pid on port 6981..."
     kill $pid
-    echo "Stopped process on port 6981."
-else
-    echo "No process found on port 6981."
-fi
+    sleep 5  # 等待5秒后重新检查端口状态
 
-sleep 5  # Wait for 5 seconds to ensure the process has been terminated
-if lsof -ti:6981; then
-    echo "Port 6981 is still in use."
-else
-    echo "Port 6981 is now free."
+    # 再次检查端口是否释放
+    pid=$(lsof -ti:6981)
+    if [ -n "$pid" ]; then
+        echo "Attempt to stop process $pid failed. Trying again..."
+    else
+        echo "Stopped process successfully. Port 6981 is now free."
+    fi
+done
+
+if [ -z "$pid" ]; then
+    echo "No process found on port 6981 or all processes successfully terminated."
 fi
 
 # 如果需要详细调试信息，可以取消以下注释以查看当前运行的Java进程
